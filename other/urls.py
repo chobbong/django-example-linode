78a63a682716c9f31b405d903a682a3553ff1d3e
조윤서 <124692313+chobbong@users.noreply.github.com>
2023-08-22 13:15:51 +0900

    map view 구현

--- other/urls.py
+++ other/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.simple_view)
+    path('search/', views.search_estate_data, name='search_estate_data'),
+    path('get_estate_data/', views.get_estate_data, name='get_estate_data'),
+
 ]
--- other/views.py
+++ other/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import PropertyData
+from django.http import JsonResponse
 
 # ######:8000/other/
 # Create your views here.
-def simple_view(request):
-    return HttpResponse("Hello, world. You're at the other index.")
+def search_estate_data(request):
+    context = {}
+    if request.method == "POST":
+        keyword = request.POST.get('search_point')
+        results = PropertyData.objects.filter(시군구__icontains=keyword) | PropertyData.objects.filter(단지명__icontains=keyword)
+        context['results'] = results
+    return render(request, 'other/search.html', context)
+
+def get_estate_data(request):
+    keyword = request.GET.get('keyword')
+    results = PropertyData.objects.filter(시군구__icontains=keyword) | PropertyData.objects.filter(단지명__icontains=keyword)
+    
+    data = []
+    for result in results:
+        data.append({
+            "lat": result.lat,
+            "lng": result.lng,
+            "시군구": result.시군구,
+            "단지명": result.단지명,
+            # 필요한 다른 필드들도 추가할 수 있습니다.
+        })
+
+    return JsonResponse(data, safe=False)
