01f1fa94299b235a9f3fd6e43fd228d01f8a36b3
조윤서 <124692313+chobbong@users.noreply.github.com>
2023-08-22 16:19:50 +0900

    지도정보표시 수정

--- other/views.py
+++ other/views.py
@@ -15,7 +15,16 @@ def search(request):
             location_data = {
                 'lat': prop.lat,
                 'lng': prop.lng,
-                'name': str(prop)  # 단지명 또는 시군구 이름을 표시하기 위해 사용
+                '단지명': prop.단지명,
+                '시군구': prop.시군구,
+                '계약연월': prop.계약연월,
+                '전용면적': prop.전용면적,
+                '매매대금_평균': prop.매매대금_평균,
+                '전세_평균': prop.전세_평균,
+                '전세가율_90': prop.전세가율_90,
+                '전세가율_80': prop.전세가율_80,
+                '전세가율_70': prop.전세가율_70,
+                '전세가율_60': prop.전세가율_60
             }
             locations.append(location_data)
 
@@ -25,4 +34,5 @@ def search(request):
     }
 
     return render(request, 'other/search.html', context)
+
 
