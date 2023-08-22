cf1612d0b953d9e8734d1a3be1ff7ba83f82dbac
조윤서 <124692313+chobbong@users.noreply.github.com>
2023-08-22 19:37:56 +0900

    search.html 모양 수정

--- other/views.py
+++ other/views.py
@@ -7,9 +7,16 @@ def search(request):
     properties = []
     locations = []  # 구글 지도에 표시할 위치 정보를 저장할 리스트
     if query:
-        properties = PropertyData.objects.filter(Q(시군구__icontains=query) | Q(단지명__icontains=query))
+        keywords = query.split()  # 공백으로 분할
+        q_objects = Q()
+
+        # 각 키워드마다 필터링 조건 생성
+        for keyword in keywords:
+            q_objects |= Q(시군구__icontains=keyword) | Q(단지명__icontains=keyword)
+
+        properties = PropertyData.objects.filter(q_objects)
         print(properties.query)  # SQL 쿼리 출력
-        
+
         for prop in properties:
             print(prop)  # 각 결과값을 출력
             location_data = {
